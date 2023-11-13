cce1efd1aed4f0723a0e504aafd800ffdc03ee26
Seth <Hellator@gmail.com>
2023-11-12 17:45:04 -0800

    Fix connection with SFML window

--- Source/Client/Modules/Network/C_NetworkConfig.vb
+++ Source/Client/Modules/Network/C_NetworkConfig.vb
@@ -7,6 +7,7 @@ Friend Module C_NetworkConfig
     Friend Sub InitNetwork()
         If Not Socket Is Nothing Then Return
         Socket = New NetworkClient(ServerPackets.COUNT, 8192)
+        Connect()
         PacketRouter()
     End Sub
 
