3796cda0e89c2c26888a8889ba08a788e2421f86
Seth <Hellator@gmail.com>
2024-03-28 05:49:38 -0500

    Update version

--- Source/Core/Configurations/Settings.vb
+++ Source/Core/Configurations/Settings.vb
@@ -32,7 +32,7 @@ Public Class Settings
     <XmlIgnore()> Public GameName As String = "MirageWorlds"
     <XmlIgnore()> Public Website As String = "https://web.miragesource.net/"
 
-    <XmlIgnore()> Public Version As String = "1.8.2"
+    <XmlIgnore()> Public Version As String = "1.8.3"
 
     Public Welcome As String = "Welcome to MirageWorlds, enjoy your stay!"
 
