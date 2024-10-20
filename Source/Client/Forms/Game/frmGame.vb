4a652ed375fc2dc553aa3bfcd6c210def116c63d
Seth <Hellator@gmail.com>
2024-10-19 23:07:20 -0500

    fix: Hide frmGame

--- Source/Client/Forms/Game/frmGame.vb
+++ Source/Client/Forms/Game/frmGame.vb
@@ -7,12 +7,12 @@ Friend Class FrmGame
 #Region "Frm"
     Private Sub FrmMainGame_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         Task.Run(Sub()
-                     Client.Run()
-                 End Sub)
+                BeginInvoke(New MethodInvoker(Sub() Hide()))
+                Startup()
+            End Sub)
 
         Task.Run(Sub()
-                     Startup()
-                     BeginInvoke(New MethodInvoker(Sub() Hide()))
+                     Client.Run()
                  End Sub)
     End Sub
 #End Region
