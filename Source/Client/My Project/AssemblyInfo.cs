5d678865ca916bb98221f8ee3b785d8768e8d983
Seth <Hellator@gmail.com>
2024-12-26 21:22:36 -0600

    2.0.0

--- Source/Client/My Project/AssemblyInfo.cs
+++ Source/Client/My Project/AssemblyInfo.cs
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // <Assembly: AssemblyVersion("1.0.*")>
 
-[assembly: AssemblyVersion("1.9.0.8")]
-[assembly: AssemblyFileVersion("1.9.0.8")]
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
