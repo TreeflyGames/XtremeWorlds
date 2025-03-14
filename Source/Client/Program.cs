f158b92d2082648a6e7399cfb098fdadcc49f836
Seth <Hellator@gmail.com>
2025-03-14 00:40:18 -0500

    fix: SharpDX

--- Source/Client/Program.cs
+++ Source/Client/Program.cs
@@ -13,7 +13,6 @@ using Keys = Microsoft.Xna.Framework.Input.Keys;
 using Point = Microsoft.Xna.Framework.Point;
 using Rectangle = Microsoft.Xna.Framework.Rectangle;
 using ButtonState = Microsoft.Xna.Framework.Input.ButtonState;
-using SharpDX.Direct2D1;
 
 namespace Client
 {
