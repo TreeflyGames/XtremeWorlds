494a8110c0d8d4a014462711b20bd74170ba7cef
Seth <Hellator@gmail.com>
2025-03-16 03:09:24 -0500

    Revert "fix: Container macOS"
    This reverts commit 45b9533eac290aeeff71f7dcc139ae7dfd2d09c2.

--- Source/Client/Game/General.cs
+++ Source/Client/Game/General.cs
@@ -2,6 +2,9 @@
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using Microsoft.VisualBasic.CompilerServices;
+using Reoria.Engine.Base.Container;
+using Reoria.Engine.Base.Container.Interfaces;
+using Reoria.Engine.Base.Container.Logging;
 using System.Runtime.InteropServices;
 using static Core.Global.Command;
 
@@ -14,9 +17,11 @@ namespace Client
         public static GameState State = new GameState();
         public static Core.Random Random = new Core.Random();
         public static Gui Gui = new Gui();
-        
+
+        public static IEngineContainer? Container;
         public static IConfiguration? Configuration;
-        
+        public static ILogger<T> GetLogger<T>() where T : class => Container?.RetrieveService<Logger<T>>() ?? throw new NullReferenceException();
+
 		[DllImport("user32.dll")]
 		public static extern bool SetForegroundWindow(IntPtr hWnd);
 
@@ -41,6 +46,16 @@ namespace Client
 
         public static void Startup()
         {
+            Container = new EngineContainer<SerilogLoggingInitializer>()
+                .DiscoverContainerServiceClasses()
+                .DiscoverConfigurationSources()
+                .BuildContainerConfiguration()
+                .BuildContainerLogger()
+                .DiscoverContainerServices()
+                .BuildContainerServices()
+                .BuildContainerServiceProvider();
+            Configuration = Container?.RetrieveService<IConfiguration>() ?? throw new NullReferenceException();
+
             GameState.InMenu = true;
             ClearGameData();
             LoadGame();
