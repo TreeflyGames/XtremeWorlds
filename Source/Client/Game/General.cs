ecaab8e7cf490ee780e34fbbdc49e2430659f434
Seth <Hellator@gmail.com>
2025-03-16 05:57:59 -0500

    fix: Engine

--- Source/Client/Game/General.cs
+++ Source/Client/Game/General.cs
@@ -2,9 +2,6 @@
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using Microsoft.VisualBasic.CompilerServices;
-using Reoria.Engine.Base.Container;
-using Reoria.Engine.Base.Container.Interfaces;
-using Reoria.Engine.Base.Container.Logging;
 using System.Runtime.InteropServices;
 using static Core.Global.Command;
 
@@ -17,11 +14,9 @@ namespace Client
         public static GameState State = new GameState();
         public static Core.Random Random = new Core.Random();
         public static Gui Gui = new Gui();
-
-        public static IEngineContainer? Container;
+        
         public static IConfiguration? Configuration;
-        public static ILogger<T> GetLogger<T>() where T : class => Container?.RetrieveService<Logger<T>>() ?? throw new NullReferenceException();
-
+        
 		public static int GetTickCount()
         {
             return Environment.TickCount;
@@ -29,16 +24,6 @@ namespace Client
 
         public static void Startup()
         {
-            Container = new EngineContainer<SerilogLoggingInitializer>()
-                .DiscoverContainerServiceClasses()
-                .DiscoverConfigurationSources()
-                .BuildContainerConfiguration()
-                .BuildContainerLogger()
-                .DiscoverContainerServices()
-                .BuildContainerServices()
-                .BuildContainerServiceProvider();
-            Configuration = Container?.RetrieveService<IConfiguration>() ?? throw new NullReferenceException();
-
             GameState.InMenu = true;
             ClearGameData();
             LoadGame();
