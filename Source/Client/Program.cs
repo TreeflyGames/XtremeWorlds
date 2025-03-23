1ea291bc403fad6b07dca555edd56c0f41b05651
Seth <Hellator@gmail.com>
2025-03-23 02:16:52 -0500

    fix: Keyboard repeat

--- Source/Client/Program.cs
+++ Source/Client/Program.cs
@@ -75,7 +75,7 @@ namespace Client
         public static Dictionary<Keys, DateTime> KeyRepeatTimers = new Dictionary<Keys, DateTime>();
 
         // Minimum interval (in milliseconds) between repeated key inputs
-        private const byte KeyRepeatInterval = 150;
+        private const byte KeyRepeatInterval = 125;
         private const byte MouseRepeatInterval = 75;
 
         // Lock object to ensure thread safety
