8feeb859b86ec9e9bdd2a3d51101fc63557b13a1
Seth <Hellator@gmail.com>
2025-03-27 14:46:18 -0500

    feat: Lock

---
+++ Source/Network/IO/Lock.cs
@@ -0,0 +1,47 @@
+﻿using System.Threading;
+using System;
+
+namespace Core
+{
+    public class Lock
+    {
+        public static readonly object Instance = new object();
+        private readonly object _lock = Instance;
+
+        public void Enter()
+        {
+            Monitor.Enter(_lock);
+        }
+
+        public void Exit()
+        {
+            Monitor.Exit(_lock);
+        }
+
+        public void Execute(Action action)
+        {
+            Enter();
+            try
+            {
+                action();
+            }
+            finally
+            {
+                Exit();
+            }
+        }
+
+        public T Execute<T>(Func<T> func)
+        {
+            Enter();
+            try
+            {
+                return func();
+            }
+            finally
+            {
+                Exit();
+            }
+        }
+    }
+}
