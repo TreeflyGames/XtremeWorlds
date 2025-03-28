7b5eaf3d18db25748ca88a3122b259cb2110e919
Seth <Hellator@gmail.com>
2025-03-28 15:31:26 -0500

    fix: Remove Reoria Client .NET 8.0

--- Source/Client/Game/General.cs
+++ Source/Client/Game/General.cs
@@ -2,9 +2,6 @@
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using Microsoft.VisualBasic.CompilerServices;
-using Reoria.Engine.Base.Container;
-using Reoria.Engine.Base.Container.Interfaces;
-using Reoria.Engine.Base.Container.Logging;
 using System.Runtime.InteropServices;
 using static Core.Global.Command;
 
@@ -17,10 +14,6 @@ namespace Client
         public static GameState State = new GameState();
         public static RandomUtility Random = new RandomUtility();
         public static Gui Gui = new Gui();
-
-        public static IEngineContainer? Container;
-        public static IConfiguration? Configuration;
-        public static ILogger<T> GetLogger<T>() where T : class => Container?.RetrieveService<Logger<T>>() ?? throw new NullReferenceException();
 
 		[DllImport("user32.dll")]
 		public static extern bool SetForegroundWindow(IntPtr hWnd);
@@ -46,16 +39,6 @@ namespace Client
 
         public static void Startup()
         {
-            Container = new EngineContainer<SerilogLoggingInitializer>()
-                .DiscoverContainerServiceClasses()
-                .DiscoverConfigurationSources()
-                .BuildContainerConfiguration()
-                .BuildContainerLogger()
-                .DiscoverContainerServices()
-                .BuildContainerServices()
-                .BuildContainerServiceProvider();
-            Configuration = Container?.RetrieveService<IConfiguration>() ?? throw new NullReferenceException();
-
             GameState.InMenu = true;
             ClearGameData();
             LoadGame();
