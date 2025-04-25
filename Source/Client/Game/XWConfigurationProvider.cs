5542925865f48fd473055695c7798e3964fb5c05
XerShade <xershade.ca@gmail.com>
2025-04-25 15:40:42 -0400

    feat: implement new overrides

--- Source/Client/Game/XWConfigurationProvider.cs
+++ Source/Client/Game/XWConfigurationProvider.cs
@@ -6,16 +6,20 @@ namespace Client;
 
 public class XWConfigurationProvider : EngineConfigurationProvider
 {
+    protected override void OnSetEarlyConfigurationBuilderBasePath(IConfigurationBuilder builder)
+        => builder.SetBasePath(AppContext.BaseDirectory);
+
     protected override void OnCreateEarlyConfigurationBuilder(IConfigurationBuilder builder)
     {
-        builder.SetBasePath(AppContext.BaseDirectory);
         _ = builder.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
         _ = builder.AddJsonFile($"appsettings.{this.Environment.ToLower()}.json", optional: true, reloadOnChange: true);
     }
 
+    protected override void OnSetConfigurationBuilderBasePath(IConfigurationBuilder builder)
+        => builder.SetBasePath(AppContext.BaseDirectory);
+
     protected override void OnCreateConfigurationBuilder(IConfigurationBuilder builder)
     {
-        builder.SetBasePath(AppContext.BaseDirectory);
         _ = builder.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
         _ = builder.AddJsonFile("appsettings.client.json", optional: true, reloadOnChange: true);
         _ = builder.AddJsonFile("appsettings.client.secret.json", optional: true, reloadOnChange: true);
