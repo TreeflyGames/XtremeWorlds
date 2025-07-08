5aa08e980ea34372f8cfcc7afd41cc432edbab28
Seth Jones <Hellator@gmail.com>
2025-07-07 18:26:13 -0700

    fix: Map, Anim, Event

--- Source/Client/Program.cs
+++ Source/Client/Program.cs
@@ -2598,9 +2598,12 @@ namespace Client
             {
                 for (i = 0; i < byte.MaxValue; i++)
                 {
-                    if (Animation.AnimInstance[i].Used[0])
+                    if (Information.UBound(Animation.AnimInstance) >= i)
                     {
-                        Animation.DrawAnimation(i, 0);
+                        if (Animation.AnimInstance[i].Used[0])
+                        {
+                            Animation.DrawAnimation(i, 0);
+                        }
                     }
                 }
             }
@@ -2741,9 +2744,12 @@ namespace Client
             {
                 for (i = 0; i < byte.MaxValue; i++)
                 {
-                    if (Animation.AnimInstance[i].Used[1])
+                    if (Information.UBound(Animation.AnimInstance) >= i)
                     {
-                        Animation.DrawAnimation(i, 1);
+                        if (Animation.AnimInstance[i].Used[1])
+                        {
+                            Animation.DrawAnimation(i, 1);
+                        }
                     }
                 }
             }
