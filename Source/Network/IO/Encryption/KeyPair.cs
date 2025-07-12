4e3b41fe90f26ee8f4b45f5c18da552e1855100f
Seth Jones <Hellator@gmail.com>
2025-07-12 14:32:03 -0700

    fix: Key Linux

--- Source/Network/IO/Encryption/KeyPair.cs
+++ Source/Network/IO/Encryption/KeyPair.cs
@@ -54,8 +54,26 @@ namespace Mirage.Sharp.Asfw.IO.Encryption
         public string ExportKeyString(bool exportPrivate = false)
         {
             CheckDisposed();
-            byte[] keyBytes = exportPrivate ? _rsa.ExportRSAPrivateKey() : _rsa.ExportRSAPublicKey();
-            return Convert.ToBase64String(keyBytes);
+
+            if (OperatingSystem.IsWindows())
+            {
+                return _rsa.ToXmlString(exportPrivate);
+            }
+            else
+            {
+                if (exportPrivate)
+                {
+                    // Export private key as PKCS#8 (Base64)
+                    var pkcs8 = _rsa.ExportPkcs8PrivateKey();
+                    return Convert.ToBase64String(pkcs8);
+                }
+                else
+                {
+                    // Export public key as X.509 SubjectPublicKeyInfo (Base64)
+                    var spki = _rsa.ExportSubjectPublicKeyInfo();
+                    return Convert.ToBase64String(spki);
+                }
+            }
         }
 
         public void ExportKey(string file, bool exportPrivate = true)
