6c3192506f9628d79a6331af66710a79376982f7
Seth <Hellator@gmail.com>
2025-07-25 00:09:02 -0500

    Revert "fix: Read-only filesystem"
    This reverts commit 7a1d1a9bac6def661f21e01710459292b39243a2.

--- Source/Core/Globals/Path.cs
+++ Source/Core/Globals/Path.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.ComponentModel;
+﻿using System.ComponentModel;
 using System.Diagnostics.SymbolStore;
 using System.IO;
 using System.Reflection;
@@ -33,20 +32,7 @@ namespace Core
         {
             get
             {
-                // macos
-                if (OperatingSystem.IsMacOS())
-                {
-                    var appData = System.IO.Path.Combine(
-                        Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData),
-                        "XtremeWorlds"
-                    );
-                    Directory.CreateDirectory(appData); // Ensure it exists
-                    return System.IO.Path.Combine(appData, "Config");
-                }
-                else
-                {
-                    return System.IO.Path.Combine(Local, "Config");
-                }
+                return System.IO.Path.Combine(Local, "Config");
             }
         }
 
